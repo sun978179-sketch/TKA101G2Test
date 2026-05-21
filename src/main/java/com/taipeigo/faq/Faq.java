81095bb8163f87a8da2c961d098e32629171c40b
Jacky Lee <sun978179@gmail.com>
2026-05-21 15:23:59 +0800

    新增faq

---
+++ src/main/java/com/taipeigo/faq/Faq.java
@@ -0,0 +1,15 @@
+package com.taipeigo.faq;
+
+public class Faq {
+	private Integer faqNo;
+
+	public Integer getFaqNo() {
+		return faqNo;
+	}
+
+	public void setFaqNo(Integer faqNo) {
+		this.faqNo = faqNo;
+	}
+	
+	
+}
