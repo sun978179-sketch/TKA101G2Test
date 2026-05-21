0641fc201c8afbf0a8c72037f5f05b280f47a37c
Jacky Lee <sun978179@gmail.com>
2026-05-21 14:48:54 +0800

    First commit

---
+++ src/main/java/com/taipeigo/member/Member.java
@@ -0,0 +1,13 @@
+package com.taipeigo.member;
+
+public class Member {
+	private Integer memNo;
+	private Integer memName;
+	
+	public Integer getMemName() {
+		return memName;
+	}
+	public void setMemName(Integer memName) {
+		this.memName = memName;
+	}
+}
